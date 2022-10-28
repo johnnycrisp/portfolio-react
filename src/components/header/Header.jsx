6bf7b1f225e57c215149535e62c756b5d75a3366
Johnny Crisp <johnnycrisp2@gmail.com>
2022-10-28 14:41:50 +0200

    initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import IMG from '../../assets/walken.png'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Johnny</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={IMG} alt="placeholder" />
+        </div> 
+        <a className="scroll-down" href="#contact">Scroll Down</a>
+      </div>   
+    </header>
+  )
+}
+
+export default Header
